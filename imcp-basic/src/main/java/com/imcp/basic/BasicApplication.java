c77ba6406e96379dc22ecf0cdbe7728bd3f01778
gaowensong <978828864@qq.com>
2020-05-29 23:11:19 +0800

    增业务代码

--- imcp-basic/src/main/java/com/imcp/basic/BasicApplication.java
+++ imcp-basic/src/main/java/com/imcp/basic/BasicApplication.java
@@ -1,6 +1,8 @@
 package com.imcp.basic;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,11 +10,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan("com.imcp.basic.*.mapper")
+@MapperScan("com.imcp.basic.mapper")
 public class BasicApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BasicApplication.class, args);
+        logger.info("========================启动完毕========================");
     }
 
 }
--- imcp-basic/src/main/java/com/imcp/basic/service/UserService.java
+++ imcp-basic/src/main/java/com/imcp/basic/service/UserService.java
@@ -2,24 +2,22 @@ package com.imcp.basic.service;
 
 import com.imcp.basic.bean.User;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional
 public interface UserService {
     //查找所有的用户
     List<User> getAllUser();
 
     //添加用户
-    void addUser(User user);
+    int addUser(User user);
 
     //根据用户名、密码查找用户
-    User getUser(@Param("username") String username, @Param("password") String password);
+    User getUser(@Param("userName") String userName, @Param("passWord") String passWord);
 
-    void delete(String guid);
+    int delete(String guid);
 
     User getUserByGuid(String guid);
 
-    void edit(User user);
+    int edit(User user);
 }
